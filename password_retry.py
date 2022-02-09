0b258a1d14ac3685a385a445de85fb3aa1ad5569
Ray Cheng <ray123321123321@gmail.com>
2022-02-08 21:26:37 -0800

    replace old one

--- password_retry.py
+++ password_retry.py
@@ -1,11 +1,14 @@
 password = 'a123456'
 i = 3
 while i > 0:
+	i = i-1
 	pwd = input('please enter your password:')
 	if pwd == password:
 		print('Sign in suceesfully')
 		break
 	else:
-		i = i-1
-		print('Wrong password, You have', i , 'more chance')
-		
+		print('Wrong password')
+		if i >0:	
+			print('You have', i , 'more chance')
+		else:
+			print('No more chance!.This account will be locked')
